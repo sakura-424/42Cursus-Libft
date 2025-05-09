a2ebba1450fdc1693d2f0b75226c74ef7c769e23
skatsuya <skatsuya@c3r5s6.42tokyo.jp>
2025-05-09 19:41:43 +0900

    fix

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,19 +6,34 @@
 /*   By: skatsuya < skatsuya@student.42tokyo.jp>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 06:47:23 by skatsuya          #+#    #+#             */
-/*   Updated: 2025/05/07 07:12:26 by skatsuya         ###   ########.fr       */
+/*   Updated: 2025/05/09 19:35:55 by skatsuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *),
-void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new_lst;
+	t_list	*new_node;
+	void	*new_content;
+
+	new_lst = NULL;
 	if (!lst || !f || !del)
 		return (NULL);
-	t_list *new_list;
-	*new_list = NULL;
-	while ()
-
+	while (lst != NULL)
+	{
+		new_content = f(lst->content);
+		new_node = malloc(sizeof(t_list));
+		if (!new_node)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		new_node->content = new_content;
+		new_node->next = NULL;
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
+	}
+	return (new_lst);
 }
