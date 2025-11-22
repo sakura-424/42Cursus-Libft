3438a3ebb1b9cafe80b6fdd38912d5212e615741
sakura-424 <sakisaki390@gmail.com>
2025-11-23 08:10:07 +0900

    使用してない変数を削除

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: skatsuya <skatsuya@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 07:21:12 by skatsuya          #+#    #+#             */
-/*   Updated: 2025/11/23 07:21:32 by skatsuya         ###   ########.fr       */
+/*   Updated: 2025/11/23 08:09:50 by skatsuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,10 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	unsigned int	i;
-
-	i = 0;
 	while (*s1 == *s2 && (*s1 != '\0') && (*s2 != '\0'))
 	{
 		s1++;
 		s2++;
-		i++;
 	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
